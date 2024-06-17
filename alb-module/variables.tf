44ec91f560c386cb2a7f32e07a45659aa6051899
Xashy-Tech <107776219+xashy-devops@users.noreply.github.com>
2024-06-16 23:44:50 -0400

    Update variables.tf

--- alb-module/variables.tf
+++ alb-module/variables.tf
@@ -1,23 +1,40 @@
-#Region
-variable "region" {
-  description = "Region of deployment."
+variable "lb_name" {
+  type        = string
+  description = "The name of the load balancer."
+}
+
+
+variable "subnets" {
+  type        = list(string)
+  description = "A list of subnet IDs to attach to the load balancer."
+}
+
+variable "target_group_name" {
+  type        = string
+  description = "The name of the target group."
+}
+
+variable "target_group_port" {
+  type        = number
+  description = "The port on which the target group is listening."
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "The VPC ID where the target group is created."
+}
+
+variable "health_check_path" {
+  type        = string
+  description = "The path for the health check endpoint."
+}
+
+variable "lbtags" {
+  type        = map(string)
+  description = "A map of tags to assign to the resources."
+}
+
+variable "listenerport" {
+  description = "port on which listener listens to incoming request"
   type = string
-}
-
-#vpc cidr
-variable "vpc_cidr" {
-  description = "cidr block of vpc."
-  type = string
-}
-
-#Subnet cidr blocks
-variable "subnet_cidr" {
-  description = "Cidr blocks of subnets"
-  type = list(string)
-}
-
-#Subnet availability zones
-variable "subnet_AZs" {
-  description = "Availability zones for subnet provisioning"
-  type = list(string)
 }
